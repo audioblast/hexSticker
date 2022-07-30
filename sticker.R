cef7e3f180ccc10959ee5a59021867a9ee681531
Ed Baker <edwbaker@gmail.com>
2022-07-30 17:49:52 +0100

    Create sticker.R

---
+++ sticker.R
@@ -0,0 +1,13 @@
+library(hexSticker)
+imgurl <- "https://cdn.audioblast.org/audioblast_flash.png"
+sticker(imgurl,
+        package="audioBlast!",
+        p_size=20,
+        p_color="black",
+        s_x=1,
+        s_y=.75,
+        s_width=.6,
+        filename="imgfile.png",
+        h_fill = "#D6D6D6",
+        h_color = "#888888"
+        )
